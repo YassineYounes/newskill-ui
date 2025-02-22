a60ba57ae8bb2352c872a209abcd03615e35dbdc
younes <yassine.younes@aderivatives.com>
2025-02-22 22:14:48 +0100

    course list page

--- src/app/components/pages/course/course-list/course-list.component.ts
+++ src/app/components/pages/course/course-list/course-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {DataService} from 'src/app/shared/service/data/data.service';
 import {MatTableDataSource} from '@angular/material/table';
 import {routes} from 'src/app/shared/service/routes/routes';
-import {courseList, latestCourses} from 'src/app/models/model';
 import {CourseService} from "../../../../services/course.service";
 import {Course} from "../../../../models/course";
 import {environment} from "../../../../../environments/environment";
@@ -24,7 +23,7 @@ export class CourseListComponent implements OnInit {
   public routes = routes;
   public selected = '1';
   public searchDataValue = '';
-  public dataSource!: MatTableDataSource<courseList>;
+  public dataSource!: MatTableDataSource<Course>;
 
   // pagination variables
   public pageSize = 10;
@@ -38,11 +37,15 @@ export class CourseListComponent implements OnInit {
   public pageSelection = [];
   public totalPages = 0;
   public courseList: Course[] = [];
-  public latestCourses: latestCourses[] = [];
+  public latestCourses: Course[] = [];
   public instructorList: Instructor[] = [];
   public categoriesList: Category[] = [];
   public freeCourses: Course[] = [];
   public payedCourses: Course[] = [];
+  public selectedCategories: string[] = [];
+  public selectedInstructors: string[] = [];
+  public selectedFreeFilter = true;
+  public selectedPayedFilter = true;
 
   constructor(private courseService: CourseService, private instructorService: InstructorService, private categoryService: CategoryService) {
   }
@@ -57,10 +60,16 @@ export class CourseListComponent implements OnInit {
     this.courseList = [];
     this.courseService.getCourseList().subscribe((res: Course[]) => {
       this.courseList = res;
+      this.latestCourses = res.slice(0, 5);
       this.totalData = res.length;
       this.calculateTotalPages(this.totalData, this.pageSize);
-      this.freeCourses = res.filter((re: Course) => {return re.isFree});
-      this.payedCourses = res.filter((re: Course) => {return !re.isFree});
+      this.freeCourses = res.filter((re: Course) => {
+        return re.isFree
+      });
+      this.payedCourses = res.filter((re: Course) => {
+        return !re.isFree
+      });
+      this.dataSource = new MatTableDataSource<Course>(this.courseList);
     });
   }
 
@@ -101,5 +110,81 @@ export class CourseListComponent implements OnInit {
 
   addCourseToWishlist(course: Course) {
     return course.inWishList = !course.inWishList;
+  }
+
+  onInstructorChange(event: any, instructorId: string | undefined): void {
+    if (!instructorId) {
+      return;
+    }
+    if (event.target.checked) {
+      this.selectedInstructors.push(instructorId);
+    } else {
+      this.selectedInstructors = this.selectedInstructors.filter(cat => cat !== instructorId);
+    }
+    this.applyFilters();
+  }
+
+  // private filterCoursesByInstructor(): void {
+  //   if (this.selectedInstructors.length === 0) {
+  //     this.dataSource.filteredData = this.courseList;
+  //   } else {
+  //     this.dataSource.filteredData = this.courseList.filter(course => {
+  //       if (!course.instructor || !course.instructor.id) {
+  //         return false;
+  //       }
+  //       console.log(this.selectedInstructors)
+  //       console.log(course.instructor.id)
+  //       return this.selectedInstructors.includes(course.instructor.id)
+  //     });
+  //   }
+  // }
+
+  onCategoryChange(event: any, categoryName: string | undefined): void {
+    if (!categoryName) {
+      return;
+    }
+    if (event.target.checked) {
+      this.selectedCategories.push(categoryName);
+    } else {
+      this.selectedCategories = this.selectedCategories.filter(cat => cat !== categoryName);
+    }
+    this.applyFilters();
+  }
+
+  public applyFilters(): void {
+    let filteredCourses = this.courseList;
+
+    // Apply instructor filter
+    if (this.selectedInstructors.length > 0) {
+      filteredCourses = filteredCourses.filter(course =>
+        course.instructor?.id && this.selectedInstructors.includes(course.instructor.id)
+      );
+    }
+
+    // Apply category filter
+    if (this.selectedCategories.length > 0) {
+      filteredCourses = filteredCourses.filter(course =>
+        course.categoriesName?.some((category: string) => this.selectedCategories.includes(category))
+      );
+    }
+
+    // Apply instructor filter
+    if (!this.selectedFreeFilter) {
+      filteredCourses = filteredCourses.filter(course => !course.isFree);
+    }
+    if (!this.selectedPayedFilter) {
+      filteredCourses = filteredCourses.filter(course => course.isFree);
+    }
+
+    // Update data source
+    this.dataSource.data = filteredCourses;
+  }
+
+  clearFilter() {
+    this.selectedPayedFilter = true;
+    this.selectedFreeFilter = true;
+    this.selectedCategories = [];
+    this.selectedInstructors = [];
+    this.applyFilters()
   }
 }
